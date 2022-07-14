7cc77b6fac41418b19db100f52aeec216418016f
ДжумабековД <@610-08>
2022-07-14 21:11:34 +0600

    Add project files.

---
+++ OnlineStoreApi/Controllers/ProductController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using OnlineStore.Model;
+using Services.Interfaces;
+
+namespace OnlineStoreApi.Controllers {
+  [Route("api/[controller]")]
+  [ApiController]
+  public class ProductController : ControllerBase 
+   {
+    private readonly IProductInterface _productService;
+    public ProductController(IProductInterface productService) {
+      _productService = productService;
+    }
+    [HttpGet("products")]
+    public async Task<IActionResult> GetProducts() {
+      var result = await _productService.GetProducts();
+      return Ok(result);
+    }
+    [HttpPut("createProduct")]
+    public async Task<IActionResult> CreateProduct([FromBody] Product product){
+      throw new NotImplementedException();
+    }
+
+    [HttpPost("updateProduct")]
+    public async Task<IActionResult> UpdateProduct([FromBody] Product product) {
+      throw new NotImplementedException();
+    }
+  }
+}
---
+++ Services.Impl/ProductService.cs
@@ -0,0 +1,37 @@
+﻿using OnlineStore.Model;
+using Services.Interfaces;
+
+namespace Services.Impl {
+  public class ProductService : IProductInterface {
+    public async Task<Product> GetProduct() {
+      return new Product() 
+      {
+        Id = 1,
+        Name = "Product 1",
+        Price = 100,
+        Description = "New Laptop1"
+      };
+    }
+
+    public async Task<List<Product>> GetProducts() {
+      return new List<Product>() 
+      {
+        new Product() 
+        {
+          Id = 1,
+          Name = "Product 1",
+          Price = 100,
+          Description = "New Laptop1"
+        },
+        new Product() 
+        {
+          Id = 2,
+          Name = "Product 2",
+          Price = 200,
+          Description = "New Laptop2"
+        },
+      };
+
+    }
+  }
+}
